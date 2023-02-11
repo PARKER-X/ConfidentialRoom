acafd46f6cb63699f1cad39152d7c1c061a13bc3
Harsh <harshrawat096@gmail.com>
2023-02-11 20:48:38 +0530

    first commit

---
+++ share/share/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from share.users.models import User
+from share.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
